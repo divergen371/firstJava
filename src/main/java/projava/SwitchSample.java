79191bf71459f3ebb334728a413b1399602d15d4
divergen371 <51857554+divergen371@users.noreply.github.com>
2023-09-21 15:54:27 +0900

    ⚗️ switchを式として書く

--- src/main/java/projava/SwitchSample.java
+++ src/main/java/projava/SwitchSample.java
@@ -3,11 +3,19 @@ package projava;
 public class SwitchSample {
     public static void main(String[] args) {
         var a = 5;
-        switch (a) {
-            case 1, 2 -> System.out.println("one-two");
-            case 3 -> System.out.println("three");
-            case 4 -> System.out.println("four");
-            case 5 -> System.out.println("five");
-        }
+//        switch (a) {
+//            case 1, 2 -> System.out.println("one-two");
+//            case 3 -> System.out.println("three");
+//            case 4 -> System.out.println("four");
+//            case 5 -> System.out.println("five");
+//            default -> System.out.println("other");
+//        }
+        System.out.println(switch (a) {
+            case 1, 2 -> "one-two";
+            case 3 -> "three";
+            case 4 -> "four";
+            case 5 -> "five";
+            default -> "other";
+        });
     }
 }
