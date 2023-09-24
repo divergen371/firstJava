e730735c8948b64d0c17e48dd1c716976bae44e8
divergen371 <51857554+divergen371@users.noreply.github.com>
2023-09-24 21:48:01 +0900

    ⚗️ 練習

--- src/main/java/projava/ForSample2.java
+++ src/main/java/projava/ForSample2.java
@@ -2,7 +2,7 @@ package projava;
 
 public class ForSample2 {
     public static void main(String[] args) {
-        for (int i = 3; i > 0; i-- ) {
+        for (int i = 0; i <= 35; i += 5 ) {
             System.out.println(i);
         }
     }
