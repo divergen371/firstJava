fbc54f3035a25c4c96a22abb858d8fae6b256796
divergen371 <51857554+divergen371@users.noreply.github.com>
2023-09-24 21:42:49 +0900

    ⚗️ カウンターに複合代入演算子をつかう

---
+++ src/main/java/projava/ForSample2.java
@@ -0,0 +1,9 @@
+package projava;
+
+public class ForSample2 {
+    public static void main(String[] args) {
+        for (int i = 0; i < 5; i += 2) {
+            System.out.println(i);
+        }
+    }
+}
