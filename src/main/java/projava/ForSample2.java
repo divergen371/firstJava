837686cd65765d1746503842ebd995a98dd4a2f7
divergen371 <51857554+divergen371@users.noreply.github.com>
2023-09-24 21:46:32 +0900

    ⚗️ カウンターを減らすパターン

--- src/main/java/projava/ForSample2.java
+++ src/main/java/projava/ForSample2.java
@@ -2,7 +2,7 @@ package projava;
 
 public class ForSample2 {
     public static void main(String[] args) {
-        for (int i = 0; i < 5; i += 2) {
+        for (int i = 3; i > 0; i-- ) {
             System.out.println(i);
         }
     }
