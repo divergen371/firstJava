72574761d649c420252ce6412ead6e30ea17f779
divergen371 <51857554+divergen371@users.noreply.github.com>
2023-09-25 21:27:24 +0900

    ⚗️ break文でループを強制終了

--- src/main/java/projava/LoopSample.java
+++ src/main/java/projava/LoopSample.java
@@ -4,8 +4,8 @@ public class LoopSample {
     public static void main(String[] args) {
         for (int i = 0; i < 5; i++) {
             if (i == 2) {
-                System.out.println("skip");
-                continue;
+                System.out.println("end");
+                break;
             }
             System.out.println(i);
         }
