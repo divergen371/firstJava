19f6663a2c4fbf521f9cd33a127feefe03fc241a
divergen371 <51857554+divergen371@users.noreply.github.com>
2023-09-25 21:22:37 +0900

    ⚗️ whileとdo while

---
+++ src/main/java/projava/DoWhileSample.java
@@ -0,0 +1,11 @@
+package projava;
+
+public class DoWhileSample {
+
+    public static void main(String[] args) {
+        int i = 5;
+        do {
+            System.out.println(i++);
+        } while (i < 5);
+    }
+}
---
+++ src/main/java/projava/WhileSample.java
@@ -0,0 +1,11 @@
+package projava;
+
+public class WhileSample {
+    public static void main(String[] args) {
+        int i = 0;
+        while (i < 5) {
+            System.out.println(i);
+            i++;
+        }
+    }
+}
