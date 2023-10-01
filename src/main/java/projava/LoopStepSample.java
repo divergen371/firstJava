5298a6396f0956ceaa3389add2fed958e5dcba4f
divergen371 <51857554+divergen371@users.noreply.github.com>
2023-10-01 17:24:26 +0900

    ⚗️ 内側のループをメソッドに分離してわかりわすくする

--- src/main/java/projava/LoopStepSample.java
+++ src/main/java/projava/LoopStepSample.java
@@ -2,10 +2,8 @@ package projava;
 
 public class LoopStepSample {
     public static void main(String[] args) {
-        for (int i = 1; i <=5; i++) {
-            for (int j = 0; j < i; j++) {
-                System.out.print("O");
-            }
+        for (int i = 1; i <= 5; i++) {
+            System.out.print("O".repeat(i));
             System.out.println();
         }
     }
