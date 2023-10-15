034366fd1e5fe6f62e57dd7153f9dfb552b4e8f5
divergen371 <51857554+divergen371@users.noreply.github.com>
2023-10-15 21:46:21 +0900

    ⚗️ 拡張For文で配列の要素を取り出す練習

---
+++ src/main/java/projava/ExtendForArrayEx.java
@@ -0,0 +1,10 @@
+package projava;
+
+public class ExtendForArrayEx {
+    public static void main(String[] args) {
+        var names = new String[]{"yusuke", "kis", "sugiyama"};
+        for (String name : names) {
+            System.out.println(name);
+        }
+    }
+}
