9cfb74481ae8e1613236a7f8fef7785a13e79c80
divergen371 <51857554+divergen371@users.noreply.github.com>
2023-10-15 21:37:21 +0900

    ⚗️ 拡張for文

--- src/main/java/projava/ForEachListSample.java
+++ src/main/java/projava/ForEachListSample.java
@@ -5,8 +5,7 @@ import java.util.List;
 public class ForEachListSample {
     public static void main(String[] args) {
         var strs = List.of("apple", "banana", "grape");
-        for (int i = 0; i < strs.size(); i++) {
-            var str = strs.get(i);
+        for (String str : strs) {
             System.out.println(str);
         }
     }
