7a6eee0ac4acc95f6c795bb75ea2e8d876875438
divergen371 <51857554+divergen371@users.noreply.github.com>
2024-01-22 23:39:47 +0900

    不要なループを除去

--- src/main/java/projava/pattern.java
+++ src/main/java/projava/pattern.java
@@ -7,14 +7,12 @@ public class pattern {
         List<String> data;
         data = List.of("apple", "banana", "grape");
         var result = false;
-        for (var str : data) {
             for (var fruit : data) {
                 if (! fruit.contains("p")) {
                     result = true;
                     break;
                 }
             }
-        }
         System.out.println(result);
     }
 }
