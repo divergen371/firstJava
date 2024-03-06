7e51c7256fed43c270341e75b975c4c144e51bec
divergen371 <51857554+divergen371@users.noreply.github.com>
2024-03-06 21:31:44 +0900

    HTTPSでアクセス

--- src/main/java/projava/WebClient.java
+++ src/main/java/projava/WebClient.java
@@ -1,20 +1,23 @@
 package projava;
 
+import javax.net.SocketFactory;
+import javax.net.ssl.SSLSocketFactory;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.io.PrintWriter;
-import java.net.Socket;
 
 public class WebClient {
     public static void main(String[] args) throws IOException {
-        var domain = "example.com";
-        try (var soc = new Socket(
+        var domain = "www.google.com";
+
+        SocketFactory factory = SSLSocketFactory.getDefault();
+        try (var soc = factory.createSocket(
                 domain,
                 80);
                 var pw = new PrintWriter(soc.getOutputStream());
                 var isr = new InputStreamReader(soc.getInputStream());
-                var bur = new BufferedReader(isr);
+                var bur = new BufferedReader(isr)
         ) {
             pw.println("GET /index.html HTTP/1.1");
             pw.println("Host: " + domain);
