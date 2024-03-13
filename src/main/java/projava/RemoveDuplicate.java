a167f791f31bcb778f5f2ae23f9fc90adeb719e3
divergen371 <51857554+divergen371@users.noreply.github.com>
2024-03-13 22:23:27 +0900

    拡張for文

--- src/main/java/projava/RemoveDuplicate.java
+++ src/main/java/projava/RemoveDuplicate.java
@@ -3,14 +3,13 @@ package projava;
 public class RemoveDuplicate {
     public static void main(String[] args) {
         var data = "abcccbaabcc";
-
+        char prev = 0;
         var builder = new StringBuilder();
-        for (int i = 0; i < data.length(); i++) {
-            char ch = data.charAt(i);
-            if (i > 0 && ch == data.charAt(i - 1)) {
-                continue;
+        for (char ch : data.toCharArray()) {
+            if (ch != prev) {
+                builder.append(ch);
+                prev = ch;
             }
-            builder.append(ch);
         }
         var result = builder.toString();
         System.out.println(data);
