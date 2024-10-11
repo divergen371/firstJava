35de29d09e8ea8b2f949605fba38b33b005ace2d
divergen371 <51857554+divergen371@users.noreply.github.com>
2024-10-12 00:56:40 +0900

    修正

--- src/main/java/practice/fortune/main_修正版.java
+++ src/main/java/practice/fortune/main_修正版.java
@@ -6,7 +6,7 @@ class Main {
         System.out.println("おみくじシステムを開始します");
         fortuneSlip.inputJudge();
         System.out.println(new StringBuilder().append(fortuneSlip.getName())
-                                              .append("さんですね！"));
+                                              .append("さんですね！こんにちは!!"));
         System.out.println(fortuneSlip.getName() + "さんの今日の運勢は" + fortuneSlip.getFortune() + "です。");
         System.out.println("よい一日を！");
         System.out.println("おみくじシステムを終了します");
