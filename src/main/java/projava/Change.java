1207e415dfc753af80c495c83c8d2090240c69e6
divergen371 <divergen371@protonmail.com>
2024-10-13 01:30:54 +0900

    ifの囲みを除去

--- src/main/java/projava/Change.java
+++ src/main/java/projava/Change.java
@@ -5,9 +5,7 @@ public class Change {
         var data = "abcdef";
         var builder = new StringBuilder();
         for (int i = 0; i < data.length(); i += 2) {
-            if (i + 1 < data.length()) {
-                builder.append(data.charAt(i + 1));
-            }
+            builder.append(data.charAt(i + 1));
             builder.append(data.charAt(i));
         }
         var result = builder.toString();
