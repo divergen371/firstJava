8fd72d018ebd97eb7ce30957460d74f2a0c5c6d5
divergen371 <user.email=51857554+divergen371@users.noreply.github.com>
2024-10-13 01:16:27 +0900

    文字列テンプレートに変換

--- src/main/java/practice/fortune/main_修正版.java
+++ src/main/java/practice/fortune/main_修正版.java
@@ -5,9 +5,8 @@ class Main {
         FortunaClass fortuneSlip = new FortunaClass();
         System.out.println("おみくじシステムを開始します。よろしくお願いします。");
         fortuneSlip.inputJudge();
-        System.out.println(new StringBuilder().append(fortuneSlip.getName())
-                                              .append("さんですね！こんにちは!!"));
-        System.out.println(fortuneSlip.getName() + "さんの今日の運勢は" + fortuneSlip.getFortune() + "です。");
+        System.out.println(STR."\{fortuneSlip.getName()}さんですね！こんにちは!!");
+        System.out.println(STR."\{fortuneSlip.getName()}さんの今日の運勢は\{fortuneSlip.getFortune()}です。");
         System.out.println("よい一日を!!");
         System.out.println("おみくじシステムを終了します!!");
     }
